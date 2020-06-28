740d89c2e82a0cace4680bc18df2cab6585c952d
ARTAUD Grégory <gregory.rtd@gmail.com>
2020-06-28 21:53:08 +0200

    ft_memcmp.c passes norminette

--- libc-funcs/ft_memcmp.c
+++ libc-funcs/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: gregory <gregory@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/28 19:57:24 by gregory           #+#    #+#             */
-/*   Updated: 2020/06/28 21:49:21 by gregory          ###   ########.fr       */
+/*   Updated: 2020/06/28 21:52:12 by gregory          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 int		ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	unsigned int cpt;
-	unsigned char *s1c;
-	unsigned char *s2c;
+	unsigned int	cpt;
+	unsigned char	*s1c;
+	unsigned char	*s2c;
 
 	s1c = (unsigned char*)s1;
 	s2c = (unsigned char*)s2;
