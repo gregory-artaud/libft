2facc45718afac1ddccedd27cb57c2871c95c24a
ARTAUD Grégory <gregory.rtd@gmail.com>
2020-06-28 14:06:23 +0200

    Added ft_memset.c in variable FILES and first version of ft_memset

--- libc-funcs/ft_memset.c
+++ libc-funcs/ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: gregory <gregory@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/28 13:56:12 by gregory           #+#    #+#             */
-/*   Updated: 2020/06/28 13:58:17 by gregory          ###   ########.fr       */
+/*   Updated: 2020/06/28 14:01:46 by gregory          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 void	*ft_memset(void *s, int c, size_t n)
 {
-	return ;
+	unsigned int cpt;
+
+	cpt = 0;
+	while (cpt < n)
+	{
+		*(char *)(s + cpt) = (char)c;
+		cpt++;
+	}
+	return (s);
 }
